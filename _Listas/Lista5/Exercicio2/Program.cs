8edf4eccbb856a4a971c1d11de23a3b7b8d7dfbf
SamukaBarbosa <samuelbardosamarciel@gmail.com>
2024-03-22 01:29:40 -0300

    Lista5

---
+++ _Listas/Lista5/Exercicio2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int MINIMO = 1;
+        const int MAXIMO = 10;
+
+        for(int i = 10; i>= MINIMO; i--)
+        {
+            for(int j = 0; j <= MAXIMO; j++)
+            {
+                System.Console.WriteLine($"{i} X {j} = {i * j}");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
