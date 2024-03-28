0c12bb74652eda046835b042500c87673dd698ef
SamukaBarbosa <samuelbardosamarciel@gmail.com>
2024-03-27 23:17:01 -0300

    Lista4

---
+++ _Listas/Lista4/Exercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int LIMITE = 9;
+
+        System.Console.WriteLine("Insira um numero: ");
+        int altura = int.Parse(Console.ReadLine());
+
+        if(altura <= LIMITE)
+        {
+            System.Console.WriteLine();
+            for (int i = 1; i <= altura; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    System.Console.Write(j);
+                }
+                System.Console.WriteLine();
+            }
+        }
+        else
+        {
+            System.Console.WriteLine("Altura invalida, isira uma altura entre 1 e 9");
+        }
+    }
+}
