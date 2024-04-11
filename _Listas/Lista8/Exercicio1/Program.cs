74cd84a1a346624036ab075e25792872efc4ee8e
SamukaBarbosa <samuelbardosamarciel@gmail.com>
2024-04-11 16:45:24 -0300

    Lista8

---
+++ _Listas/Lista8/Exercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+struct Pizza
+{
+    public int id;
+    public string sabor;
+    public double preco;
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza pizza;
+
+        pizza.id = int.Parse(Console.ReadLine());
+        pizza.sabor = Console.ReadLine();
+        pizza.preco = double.Parse(Console.ReadLine());
+   
+        System.Console.WriteLine(pizza.id);
+        System.Console.WriteLine(pizza.sabor);
+        System.Console.WriteLine(pizza.preco);
+    }
+}
